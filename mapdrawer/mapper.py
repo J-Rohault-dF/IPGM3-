354e85bafaf13b834f29261efba0478538a4a19f
Julio974-TheHexahedron <Jules.bour.1@gmail.com>
2021-11-06 09:31:38 +0100

    Ignore empty (for approval maps)

--- mapdrawer/mapper.py
+++ mapdrawer/mapper.py
@@ -7,44 +7,45 @@ from ipgm.ResultsSet import *
 from mapdrawer.colors import *
 
 def getMaxK(d):
-    k = ''
-    v = float('-inf')
-    for i in d.keys():
-        if d[i] > v:
-            v = d[i]
-            k = i
-    return k
+	k = ''
+	v = float('-inf')
+	for i in d.keys():
+		if d[i] > v:
+			v = d[i]
+			k = i
+	return k
 
 def getWinningColor(d: dict, partiesColors: dict) -> str:
-    k = getMaxK(d)
-    kp = d[k]
-    indexInTable = math.floor(kp*20)-2
+	k = getMaxK(d)
+	if k == '': k = getMaxK({k: v for i,v in d.items() if k != ''})
+	kp = d[k]
+	indexInTable = math.floor(kp*20)-2
 
-    if hasPartyColor(k, partiesColors):
-        return getShade(getPartyColor(k, partiesColors), indexInTable).hex_l[1:]
-    else:
-        print('missing color for {0} ({1}%)'.format(k, kp))
-        return '000000'
+	if hasPartyColor(k, partiesColors):
+		return getShade(getPartyColor(k, partiesColors), indexInTable).hex_l[1:]
+	else:
+		print('missing color for {0} ({1}%)'.format(k, kp))
+		return '000000'
 
 def getWinningColorR(res: Result, partiesColors: dict) -> str:
-    if res == None: return '000000'
-    else: return getWinningColor(res.toPercentages().removedAbs().results, partiesColors)
+	if res == None: return '000000'
+	else: return getWinningColor(res.toPercentages().removedAbs().results, partiesColors)
 
 def getWinningColorP(d: dict[str, float], partiesColors: dict) -> str:
-    if d == None: return '000000'
+	if d == None: return '000000'
 
-    k1, m = getProbsFromResDict(d)
-    
-    if m > 0.9: indexInTable = 11
-    elif m > 0.6: indexInTable = 8
-    elif m > 0.3: indexInTable = 5
-    else: return '000000'
+	k1, m = getProbsFromResDict(d)
+	
+	if m > 0.9: indexInTable = 11
+	elif m > 0.6: indexInTable = 8
+	elif m > 0.3: indexInTable = 5
+	else: return '000000'
 
-    if hasPartyColor(k1, partiesColors):
-        return getShade(getPartyColor(k1, partiesColors), indexInTable).hex_l[1:]
-    else:
-        print('missing color for {0} ({1}%)'.format(k1, d[k1]))
-        return '000000'
+	if hasPartyColor(k1, partiesColors):
+		return getShade(getPartyColor(k1, partiesColors), indexInTable).hex_l[1:]
+	else:
+		print('missing color for {0} ({1}%)'.format(k1, d[k1]))
+		return '000000'
 
 
 
