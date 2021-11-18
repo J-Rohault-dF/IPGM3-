d9476ac84c0f68895251d82c64cd652b12e8f1a5
Julio974-Theexahedron <jules.bour.1@gmail.com>
2021-11-18 18:04:04 +0100

    Optimized keydrawer

--- mapdrawer/keydrawer.py
+++ mapdrawer/keydrawer.py
@@ -47,63 +47,11 @@ def drawCandidate(name: str, score: float):
 #Draw score ring behind
 def drawPercRingBehind(centerPoint: set[float, float], innerRadius: float, outerRadius: float, factor: float, fillColor: Color = Color('#000000')):
 	
-	cx,cy = centerPoint[0], centerPoint[1]
-	
 	margin = (outerRadius-innerRadius)*factor
 	innerRadius -= margin
 	outerRadius += margin
 
-	maskId = getRandomAlphanumeric(6)
-	mask: list[etree.Element] = []
-	mask.append(etree.Element('{http://www.inkscape.org/namespaces/inkscape}path-effect', attrib={
-		'effect': 'powermask',
-		'id': 'path-effect-{id}'.format(id=maskId),
-		'is_visible': 'true',
-		'lpeversion': '1',
-		'uri': '#mask-powermask-path-effect-{id}'.format(id=maskId),
-		'invert': 'false',
-		'hide_mask': 'false',
-		'background': 'true',
-		'background_color': '#ffffffff',
-	}))
-	mask.append(etree.Element('{http://www.w3.org/2000/svg}mask', attrib={
-		'maskUnits': 'userSpaceOnUse',
-		'id': 'mask-powermask-path-effect-{id}'.format(id=maskId),
-	}))
-	mask[1].append(etree.Element('{http://www.w3.org/2000/svg}path', attrib={
-		'id': 'mask-powermask-path-effect-{id}_box'.format(id=maskId),
-		'style': 'fill:#ffffff;fill-opacity:1',
-		'd': 'M {cxmo},{cymo} h {o2} v {o2} h {no2} z'.format(o2=(outerRadius*2), no2=-(outerRadius*2), cxmo=(cx-outerRadius), cymo=(cy-outerRadius)),
-	}))
-	mask[1].append(etree.Element('{http://www.w3.org/2000/svg}circle', attrib={
-		'style': 'fill:#000000;stroke:none;stroke-width:5;stroke-linejoin:round',
-		'id': 'circle-mask-{id}'.format(id=maskId),
-		'cx': str(cx),
-		'cy': str(cy),
-		'r': str(innerRadius),
-		'd': 'm {cxpi},{cy} a {r},{r} 0 0 1 -{r},{r} {r},{r} 0 0 1 -{r},-{r} {r},{r} 0 0 1 {r},-{r} {r},{r} 0 0 1 {r},{r} z'.format(cx=cx, cy=cy, r=innerRadius, cxpi=(cx+innerRadius)),
-	}))
-
-	mask.append(etree.Element('{http://www.w3.org/2000/svg}filter', attrib={
-		'id': 'mask-powermask-path-effect-{id}_inverse'.format(id=maskId),
-		'{http://www.inkscape.org/namespaces/inkscape}label': 'filtermask-powermask-path-effect-{id}'.format(id=maskId),
-		'style': 'color-interpolation-filters:sRGB',
-		'height': '100',
-		'width': '100',
-		'x': '-50',
-		'y': '-50',
-	}))
-	mask[2].append(etree.Element('{http://www.w3.org/2000/svg}feColorMatrix', attrib={
-		'id': 'mask-powermask-path-effect-{id}_primitive1'.format(id=maskId),
-		'values': '1',
-		'type': 'saturate',
-		'result': 'fbSourceGraphic',
-	}))
-	mask[2].append(etree.Element('{http://www.w3.org/2000/svg}feColorMatrix', attrib={
-		'id': 'mask-powermask-path-effect-{id}_primitive2'.format(id=maskId),
-		'values': '-1 0 0 0 1 0 -1 0 0 1 0 0 -1 0 1 0 0 0 1 0 ',
-		'in': 'fbSourceGraphic',
-	}))
+	maskId, mask = createMask(centerPoint, innerRadius, outerRadius)
 
 	return (mask, etree.Element('{http://www.w3.org/2000/svg}path', attrib={
 		'd': 'm 88.211234,12.28561 a 5,5 0 0 1 4.390368,2.614692 5,5 0 0 1 -0.199433,5.106091 l -4.194916,-2.720785 z',
@@ -119,7 +67,8 @@ def drawPercRingBehind(centerPoint: set[float, float], innerRadius: float, outer
 		'{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}arc-type': 'slice',
 		'style': 'fill:{color};stroke:none;stroke-width:5;stroke-linejoin:round'.format(color=fillColor),
 	}))
-	
+
+
 
 #Draw score ring
 def drawPercRing(centerPoint: set[float, float], outerRadius: float, innerRadius: float, percs: list[float], colors: list[Color]) -> set[list[etree.Element], etree.Element]:
@@ -140,58 +89,7 @@ def drawPercRing(centerPoint: set[float, float], outerRadius: float, innerRadius
 		) )
 		run += percs[i]
 	
-	#Make the mask
-	maskId = getRandomAlphanumeric(6)
-	mask: list[etree.Element] = []
-	mask.append(etree.Element('{http://www.inkscape.org/namespaces/inkscape}path-effect', attrib={
-		'effect': 'powermask',
-		'id': 'path-effect-{id}'.format(id=maskId),
-		'is_visible': 'true',
-		'lpeversion': '1',
-		'uri': '#mask-powermask-path-effect-{id}'.format(id=maskId),
-		'invert': 'false',
-		'hide_mask': 'false',
-		'background': 'true',
-		'background_color': '#ffffffff',
-	}))
-	mask.append(etree.Element('{http://www.w3.org/2000/svg}mask', attrib={
-		'maskUnits': 'userSpaceOnUse',
-		'id': 'mask-powermask-path-effect-{id}'.format(id=maskId),
-	}))
-	mask[1].append(etree.Element('{http://www.w3.org/2000/svg}path', attrib={
-		'id': 'mask-powermask-path-effect-{id}_box'.format(id=maskId),
-		'style': 'fill:#ffffff;fill-opacity:1',
-		'd': 'M {cxmo},{cymo} h {o2} v {o2} h {no2} z'.format(o2=(outerRadius*2), no2=-(outerRadius*2), cxmo=(cx-outerRadius), cymo=(cy-outerRadius)),
-	}))
-	mask[1].append(etree.Element('{http://www.w3.org/2000/svg}circle', attrib={
-		'style': 'fill:#000000;stroke:none;stroke-width:5;stroke-linejoin:round',
-		'id': 'circle-mask-{id}'.format(id=maskId),
-		'cx': str(cx),
-		'cy': str(cy),
-		'r': str(innerRadius),
-		'd': 'm {cxpi},{cy} a {r},{r} 0 0 1 -{r},{r} {r},{r} 0 0 1 -{r},-{r} {r},{r} 0 0 1 {r},-{r} {r},{r} 0 0 1 {r},{r} z'.format(cx=cx, cy=cy, r=innerRadius, cxpi=(cx+innerRadius)),
-	}))
-
-	mask.append(etree.Element('{http://www.w3.org/2000/svg}filter', attrib={
-		'id': 'mask-powermask-path-effect-{id}_inverse'.format(id=maskId),
-		'{http://www.inkscape.org/namespaces/inkscape}label': 'filtermask-powermask-path-effect-{id}'.format(id=maskId),
-		'style': 'color-interpolation-filters:sRGB',
-		'height': '100',
-		'width': '100',
-		'x': '-50',
-		'y': '-50',
-	}))
-	mask[2].append(etree.Element('{http://www.w3.org/2000/svg}feColorMatrix', attrib={
-		'id': 'mask-powermask-path-effect-{id}_primitive1'.format(id=maskId),
-		'values': '1',
-		'type': 'saturate',
-		'result': 'fbSourceGraphic',
-	}))
-	mask[2].append(etree.Element('{http://www.w3.org/2000/svg}feColorMatrix', attrib={
-		'id': 'mask-powermask-path-effect-{id}_primitive2'.format(id=maskId),
-		'values': '-1 0 0 0 1 0 -1 0 0 1 0 0 -1 0 1 0 0 0 1 0 ',
-		'in': 'fbSourceGraphic',
-	}))
+	maskId, mask = createMask(centerPoint, innerRadius, outerRadius)
 
 	#Then convert that to the texts
 	ringSegments = etree.Element('{http://www.w3.org/2000/svg}g', attrib={
@@ -217,3 +115,65 @@ def drawPercRing(centerPoint: set[float, float], outerRadius: float, innerRadius
 	#Then return that along with the mask
 	return (mask, ringSegments)
 
+
+
+
+
+def createMask(centerPoint: set[float, float], innerRadius: float, outerRadius: float):
+	
+	cx,cy = centerPoint[0],centerPoint[1]
+
+	maskId = getRandomAlphanumeric(6)
+
+	mask: list[etree.Element] = []
+	mask.append(etree.Element('{http://www.inkscape.org/namespaces/inkscape}path-effect', attrib={
+		'effect': 'powermask',
+		'id': 'path-effect-{id}'.format(id=maskId),
+		'is_visible': 'true',
+		'lpeversion': '1',
+		'uri': '#mask-powermask-path-effect-{id}'.format(id=maskId),
+		'invert': 'false',
+		'hide_mask': 'false',
+		'background': 'true',
+		'background_color': '#ffffffff',
+	}))
+	mask.append(etree.Element('{http://www.w3.org/2000/svg}mask', attrib={
+		'maskUnits': 'userSpaceOnUse',
+		'id': 'mask-powermask-path-effect-{id}'.format(id=maskId),
+	}))
+	mask[1].append(etree.Element('{http://www.w3.org/2000/svg}path', attrib={
+		'id': 'mask-powermask-path-effect-{id}_box'.format(id=maskId),
+		'style': 'fill:#ffffff;fill-opacity:1',
+		'd': 'M {cxmo},{cymo} h {o2} v {o2} h {no2} z'.format(o2=(outerRadius*2), no2=-(outerRadius*2), cxmo=(cx-outerRadius), cymo=(cy-outerRadius)),
+	}))
+	mask[1].append(etree.Element('{http://www.w3.org/2000/svg}circle', attrib={
+		'style': 'fill:#000000;stroke:none;stroke-width:5;stroke-linejoin:round',
+		'id': 'circle-mask-{id}'.format(id=maskId),
+		'cx': str(cx),
+		'cy': str(cy),
+		'r': str(innerRadius),
+		'd': 'm {cxpi},{cy} a {r},{r} 0 0 1 -{r},{r} {r},{r} 0 0 1 -{r},-{r} {r},{r} 0 0 1 {r},-{r} {r},{r} 0 0 1 {r},{r} z'.format(cx=cx, cy=cy, r=innerRadius, cxpi=(cx+innerRadius)),
+	}))
+
+	mask.append(etree.Element('{http://www.w3.org/2000/svg}filter', attrib={
+		'id': 'mask-powermask-path-effect-{id}_inverse'.format(id=maskId),
+		'{http://www.inkscape.org/namespaces/inkscape}label': 'filtermask-powermask-path-effect-{id}'.format(id=maskId),
+		'style': 'color-interpolation-filters:sRGB',
+		'height': '100',
+		'width': '100',
+		'x': '-50',
+		'y': '-50',
+	}))
+	mask[2].append(etree.Element('{http://www.w3.org/2000/svg}feColorMatrix', attrib={
+		'id': 'mask-powermask-path-effect-{id}_primitive1'.format(id=maskId),
+		'values': '1',
+		'type': 'saturate',
+		'result': 'fbSourceGraphic',
+	}))
+	mask[2].append(etree.Element('{http://www.w3.org/2000/svg}feColorMatrix', attrib={
+		'id': 'mask-powermask-path-effect-{id}_primitive2'.format(id=maskId),
+		'values': '-1 0 0 0 1 0 -1 0 0 1 0 0 -1 0 1 0 0 0 1 0 ',
+		'in': 'fbSourceGraphic',
+	}))
+	
+	return maskId,mask
