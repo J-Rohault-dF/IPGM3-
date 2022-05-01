b4b67449398e609fac20c2bd3c16ce9f3c41cfcc
J-Rohault-dF <jules.bour.1@gmail.com>
2022-05-01 13:08:27 +0200

    Added more proportional maps and stuff

--- ipgm3_fr_ll.py
+++ ipgm3_fr_ll.py
@@ -14,13 +14,31 @@ with open('data/rings_fr.csv','r',encoding='utf8') as seatsDataFile:
 	ringsDataTemp = [y.split(';') for y in [x for x in seatsDataFile.read().split('\n')]]
 	ringsData = {x[0]: dict(zip(ringsDataTemp[0][1:], [toFloatOrStr(y) for y in x[1:]])) for x in ringsDataTemp[1:]}
 
-#Get seats data
+#Get seats data DEPTS
 with open('data/seats_fr_depts.csv','r',encoding='utf8') as seatsDataFile:
 	seatsDataTemp = [y.split(';') for y in [x for x in seatsDataFile.read().split('\n')]]
-	seatsData = {x[0]: dict(zip(seatsDataTemp[0][1:], [toFloatOrStr(y) for y in x[1:]])) for x in seatsDataTemp[1:]}
-seatsPerDept = {k: v['seats'] for k,v in seatsData.items()}
+	seatsDataDepts = {x[0]: dict(zip(seatsDataTemp[0][1:], [toFloatOrStr(y) for y in x[1:]])) for x in seatsDataTemp[1:]}
+seatsPerDept = {k: v['seats'] for k,v in seatsDataDepts.items()}
 
-allDivs = AllDivs('data/divs_fr.txt')
+#Get seats data CONST
+with open('data/seats_fr_canard.csv','r',encoding='utf8') as seatsDataFile:
+	seatsDataTemp = [y.split(';') for y in [x for x in seatsDataFile.read().split('\n')]]
+	seatsDataConst = {x[0]: dict(zip(seatsDataTemp[0][1:], [toFloatOrStr(y) for y in x[1:]])) for x in seatsDataTemp[1:]}
+seatsPerConst = {k: v['seats'] for k,v in seatsDataConst.items()}
+
+#Get seats data OLDREGIONS
+with open('data/seats_fr_oldRegions.csv','r',encoding='utf8') as seatsDataFile:
+	seatsDataTemp = [y.split(';') for y in [x for x in seatsDataFile.read().split('\n')]]
+	seatsDataOldRegions = {x[0]: dict(zip(seatsDataTemp[0][1:], [toFloatOrStr(y) for y in x[1:]])) for x in seatsDataTemp[1:]}
+seatsPerOldRegion = {k: v['seats'] for k,v in seatsDataOldRegions.items()}
+
+#Get seats data REGIONS
+with open('data/seats_fr_regions.csv','r',encoding='utf8') as seatsDataFile:
+	seatsDataTemp = [y.split(';') for y in [x for x in seatsDataFile.read().split('\n')]]
+	seatsDataRegions = {x[0]: dict(zip(seatsDataTemp[0][1:], [toFloatOrStr(y) for y in x[1:]])) for x in seatsDataTemp[1:]}
+seatsPerRegion = {k: v['seats'] for k,v in seatsDataRegions.items()}
+
+allDivs = AllDivs('data/divs_fr_prop.txt')
 
 t1_2017 = loadDataTable('data/stats_fr/2017T1.csv', allDivs)
 t2_2017 = loadDataTable('data/stats_fr/2017T2.csv', allDivs)
@@ -38,9 +56,9 @@ candidaciesData: Candidacies = importCandidacies(srcParties='data/parties_fr.csv
 
 
 
-doExportTxt = True
+doExportTxt = False
 doExportMap = True
-doExportCsv = True
+doExportCsv = False
 
 allRounds = {}
 allTexts = []
@@ -70,8 +88,17 @@ for hk, hv in {k: v for k,v in mx.items() if k != 'sampleSize'}.items():
 	allRounds[hk] = r
 
 	#Compute the seats count
-	seatsParties = {x: proportionalHighestAverage(filterThreshold(r.get(x).result), seatsPerDept[x], 'D\'Hondt') for x in seatsPerDept.keys()}
-	print(hk, {k: sum([xv[k] for xk,xv in seatsParties.items()]) for k,v in r.result.results.items() if isExpressed(k)})
+	seatsPartiesDepts = {x: proportionalHighestAverage(filterThreshold(r.get(x).result), seatsPerDept[x], 'D\'Hondt') for x in seatsPerDept.keys()}
+	print(hk, 'seatsPartiesDepts', {k: sum([xv[k] for xk,xv in seatsPartiesDepts.items()]) for k,v in r.result.results.items() if isExpressed(k)})
+
+	seatsPartiesConst = {x: proportionalHighestAverage(filterThreshold(r.get(x).result), seatsPerConst[x], 'D\'Hondt') for x in seatsPerConst.keys()}
+	print(hk, 'seatsPartiesConst', {k: sum([xv[k] for xk,xv in seatsPartiesConst.items()]) for k,v in r.result.results.items() if isExpressed(k)})
+
+	seatsPartiesOldRegions = {x: proportionalHighestAverage(filterThreshold(r.get(x).result), seatsPerOldRegion[x], 'D\'Hondt') for x in seatsPerOldRegion.keys()}
+	print(hk, 'seatsPartiesOldRegions', {k: sum([xv[k] for xk,xv in seatsPartiesOldRegions.items()]) for k,v in r.result.results.items() if isExpressed(k)})
+
+	seatsPartiesRegions = {x: proportionalHighestAverage(filterThreshold(r.get(x).result), seatsPerRegion[x], 'D\'Hondt') for x in seatsPerRegion.keys()}
+	print(hk, 'seatsPartiesRegions', {k: sum([xv[k] for xk,xv in seatsPartiesRegions.items()]) for k,v in r.result.results.items() if isExpressed(k)})
 
 	#Tweet text
 	if doExportTxt: allTexts.append('HYPOTHESIS {h}\n'.format(h=hk)+makeTweetText(r.result.toPercentages(), hv['sampleSize'], top=1, nbSimulated=15000, candidaciesData=candidaciesData, threshold=0.05))
@@ -81,7 +108,10 @@ for hk, hv in {k: v for k,v in mx.items() if k != 'sampleSize'}.items():
 	if doExportMap:
 		exportMap(r, 'data/basemap_collectivites_gparis.svg', '{path}/{h}.svg'.format(h=hk, path=poll), candidaciesData=candidaciesData)
 		exportMap(r, 'data/basemap_collectivites_gparis.svg', '{path}/{h}_r.svg'.format(h=hk, path=poll), candidaciesData=candidaciesData, doRings=True, ringsData=ringsData, outerRadius=(5*10), innerRadius=(3*10))
-		exportSeatsMap(r, seatsParties, seatsData, 'data/basemap_collectivites_gparis.svg', '{path}/{h}_s.svg'.format(h=hk, path=poll), allDivs=allDivs, candidaciesData=candidaciesData, seatsScale=5, mapScaling=3)
+		exportSeatsMap(r, seatsPartiesDepts, seatsDataDepts, 'data/basemap_collectivites_gparis.svg', '{path}/{h}_s_d.svg'.format(h=hk, path=poll), allDivs=allDivs, candidaciesData=candidaciesData, seatsScale=5, mapScaling=3)
+		exportSeatsMap(r, seatsPartiesConst, seatsDataConst, 'data/basemap_fr_circonscriptions_canard.svg', '{path}/{h}_s_c.svg'.format(h=hk, path=poll), allDivs=allDivs, candidaciesData=candidaciesData, seatsScale=5, mapScaling=3)
+		exportSeatsMap(r, seatsPartiesOldRegions, seatsDataOldRegions, 'data/basemap_fr_oldRegions.svg', '{path}/{h}_s_or.svg'.format(h=hk, path=poll), allDivs=allDivs, candidaciesData=candidaciesData, seatsScale=5, mapScaling=3)
+		exportSeatsMap(r, seatsPartiesRegions, seatsDataRegions, 'data/basemap_fr_regions.svg', '{path}/{h}_s_r.svg'.format(h=hk, path=poll), allDivs=allDivs, candidaciesData=candidaciesData, seatsScale=5, mapScaling=3)
 
 if doExportTxt:
 	with open('exports/{path}/tweetText.txt'.format(path=poll),'w',encoding='utf8') as txtFile:
