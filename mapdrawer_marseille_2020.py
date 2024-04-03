a6d27e8fbe42516e22317bced0d58d366eddb518
J-Rohault-dF <jules.bour.1@gmail.com>
2024-04-03 22:31:02 +0200

    Added 2020 Marseille municipal election

---
+++ mapdrawer_marseille_2020.py
@@ -0,0 +1,55 @@
+import os
+import copy
+
+from ipgm.utils import *
+from ipgm.port import *
+from ipgm.mainFuncs import *
+from twitterTextAdditions import *
+from divsHandler import *
+from mapdrawer.mapper import *
+
+#Get divs data
+#with open('data/marseille/rings/arrondissements.csv','r',encoding='utf8') as seatsDataFile:
+#	ringsDataTemp = [y.split(';') for y in [x for x in seatsDataFile.read().split('\n')]]
+#	ringsData = {x[0]: dict(zip(ringsDataTemp[0][1:], [toFloatOrStr(y) for y in x[1:]])) for x in ringsDataTemp[1:]}
+
+allDivs = AllDivs('data/marseille/divs/marseille_2020.txt')
+
+t1_2020 = importDataTable('data/marseille/stats/2020T1.csv', allDivs)
+t2_2020 = importDataTable('data/marseille/stats/2020T2.csv', allDivs)
+
+candidaciesData: Candidacies = importCandidacies(src='data/marseille/cands/2020.csv')
+
+exportMap(t1_2020, 'data/marseille/maps/bureaux_de_vote_2023.svg', 'marseille_2020/2020T1_b.svg', candidaciesData=candidaciesData)
+exportMap(t1_2020, 'data/marseille/maps/arrondissements.svg',      'marseille_2020/2020T1_a.svg', candidaciesData=candidaciesData)
+exportMap(t1_2020, 'data/marseille/maps/secteurs.svg',             'marseille_2020/2020T1_s.svg', candidaciesData=candidaciesData)
+
+exportMap(t2_2020, 'data/marseille/maps/bureaux_de_vote_2023.svg', 'marseille_2020/2020T2_b.svg', candidaciesData=candidaciesData)
+exportMap(t2_2020, 'data/marseille/maps/arrondissements.svg',      'marseille_2020/2020T2_a.svg', candidaciesData=candidaciesData)
+exportMap(t2_2020, 'data/marseille/maps/secteurs.svg',             'marseille_2020/2020T2_s.svg', candidaciesData=candidaciesData)
+
+#Make individual maps for each party's scores (warning: very hacky bodge, candidates with a '#' at the start of their names are ignored for coloring purposes
+candidatesList = [x.shortName for x in candidaciesData.listOfCands]
+for candidate in candidatesList:
+	t1_2020.renameCandidate(candidate, '#'+candidate)
+	t2_2020.renameCandidate(candidate, '#'+candidate)
+
+for candidate in candidatesList:
+	if '#'+candidate not in t1_2020.result.results: continue
+	print(candidate)
+	t1_2020.renameCandidate('#'+candidate, candidate)
+	scoreMultiplier = 1 / t1_2020.maximumScore(candidate)
+	exportMap(t1_2020, 'data/marseille/maps/bureaux_de_vote_2023.svg', 'marseille_2020/T1_{candidate}_b.svg'.format(candidate=candidate), candidaciesData=candidaciesData, scoreMultiplier=scoreMultiplier)
+	exportMap(t1_2020, 'data/marseille/maps/arrondissements.svg',      'marseille_2020/T1_{candidate}_a.svg'.format(candidate=candidate), candidaciesData=candidaciesData, scoreMultiplier=scoreMultiplier)
+	exportMap(t1_2020, 'data/marseille/maps/secteurs.svg',             'marseille_2020/T1_{candidate}_s.svg'.format(candidate=candidate), candidaciesData=candidaciesData, scoreMultiplier=scoreMultiplier)
+	t1_2020.renameCandidate(candidate, '#'+candidate)
+
+for candidate in candidatesList:
+	if '#'+candidate not in t2_2020.result.results: continue
+	print(candidate)
+	t2_2020.renameCandidate('#'+candidate, candidate)
+	scoreMultiplier = 1 / t2_2020.maximumScore(candidate)
+	exportMap(t2_2020, 'data/marseille/maps/bureaux_de_vote_2023.svg', 'marseille_2020/T2_{candidate}_b.svg'.format(candidate=candidate), candidaciesData=candidaciesData, scoreMultiplier=scoreMultiplier)
+	exportMap(t2_2020, 'data/marseille/maps/arrondissements.svg',      'marseille_2020/T2_{candidate}_a.svg'.format(candidate=candidate), candidaciesData=candidaciesData, scoreMultiplier=scoreMultiplier)
+	exportMap(t2_2020, 'data/marseille/maps/secteurs.svg',             'marseille_2020/T2_{candidate}_s.svg'.format(candidate=candidate), candidaciesData=candidaciesData, scoreMultiplier=scoreMultiplier)
+	t2_2020.renameCandidate(candidate, '#'+candidate)
